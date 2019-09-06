46098ab826e1992997309f48f0337872c763c27d
zee <zzdd9251@gmail.com>
2019-09-06 11:03:55 -0700

    sprint started..

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,60 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+     console.log(response.data);
+  let x = response.data;
+  
+    
+    cotainer.appendChild(createCard(response.data));
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+
+
+const cotainer = document.querySelector(".carousel-container");
+
+function createCard(data) {
+  const card = document.createElement("div"),
+    headLine = document.createElement("div"),
+    author = document.createElement("div"),
+    imgdiv = document.createElement("div"),
+    img = document.createElement("img"),
+    span = document.createElement("span");
+
+   card.classList.add('card')
+   headLine.classList.add('headLine')
+   author.classList.add('author')
+   imgdiv.classList.add("img-container")
+
+   card.appendChild(headLine)
+   card.appendChild(author)
+   author.appendChild(imgdiv)
+   imgdiv.appendChild(img)
+   author.appendChild(span)
+
+    data.forEach(item => {
+        headLine.textContent = item.articles;
+        img.src = item.articles;
+        span.textContent = item.articles;
+       });
+  
+  
+
+    console.log(data.articles.bootstrap);
+
+return card
+
+}
+
+// console.log(cotainer)
+
+
+
+   
+
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,41 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+entryDiv=document.querySelector('.topics')
+function topic(){
+    const topicsDiv=document.createElement('div')
+    topicsDiv.classList.add('tab')
+
+    // topicsDiv.textContent
+
+
+    // console.log(x[3])
+    return topicsDiv
+}
+// entryDiv.appendChild(topic());
+console.log(entryDiv);
+
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    // console.log(response.data.topics);
+    let x = response.data.topics;
+    x.forEach(item => {
+        const newTopic = topic(item);
+        entryDiv.appendChild(newTopic);
+        let b=entryDiv.querySelectorAll('.tab')
+        
+        //  console.log(b);
+      
+     });
+      //  const tab = Document.querySelectorAll(".tab");
+      //  console.log(tab);
+       
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+// 
+
+// console.log(x)
